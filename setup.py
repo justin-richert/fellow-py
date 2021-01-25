e715d7a626e7cb1dc91b473cdb090f93523740f3
Justin Richert <justin.richert@life.church>
2021-01-24 21:56:40 -0600

    Fix long description in setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     install_requires=["bleak>=0.10.0"],
     license="MIT License",
     long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="fellow",
     name="fellow-py",
