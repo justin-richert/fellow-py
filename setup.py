4811bc4e09ade15ae85e56935042885cbed652c3
Justin Richert <justin.richert@life.church>
2021-01-24 22:08:06 -0600

    bump version

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     test_suite="tests",
     tests_require=["pytest>=3"],
     url="https://github.com/justin-richert/fellow-py",
-    version="0.1.0",
+    version="0.1.1",
     zip_safe=False,
 )
